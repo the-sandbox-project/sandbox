729964c7d7e537bdd5cff4214b67d57eb36fc7d6
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-24 18:47:12 +0100

    Feat(lib.rs): Implementation of Search & Install Functions

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,17 @@ mod config;
 mod args;
 mod environment;
 mod search;
+mod install;
 
 use args::SandboxArgs;
 use clap::Parser;
-use environment::setup_environment;
+use install::install_environment;
+use search::search_environment;
 
 pub fn run() { 
     let args = SandboxArgs::parse();
-    setup_environment(args.beach_type)
+    // setup_environment(args.beach_type)
+    // search_environment("minimal".to_string())
+    install_environment("go-min".to_string())
 
 }
