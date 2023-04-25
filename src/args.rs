4eba6a9461d1506413782a257406f8ee0c92ef89
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-25 16:28:28 +0100

    Feat(args.rs): Added Search & Install Arguments

--- src/args.rs
+++ src/args.rs
@@ -2,10 +2,17 @@ use clap::Parser;
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
-
 pub struct SandboxArgs {
     /// Create a New Beach
-    #[clap(short = 'n', long = "new")]
+    #[clap(short = 'n', long = "new", default_value = "")]
     pub beach_type: String,
+
+    /// Search for Environment
+    #[clap(short, long, default_value = "")]
+    pub search: String,
+
+    /// Search for Environment
+    #[clap(short, long, default_value = "")]
+    pub install: String,
 }
 
