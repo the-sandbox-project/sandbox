7b4972dd146f022e27e6a3564d345a390de900d1
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-25 16:26:55 +0100

    Feat(lib.rs): Implemented Argument Parsing

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,22 @@ mod install;
 use args::SandboxArgs;
 use clap::Parser;
 use install::install_environment;
-use search::search_environment;
+use search::search;
+
+use crate::environment::setup_environment;
 
 pub fn run() { 
     let args = SandboxArgs::parse();
-    // setup_environment(args.beach_type)
-    // search_environment("minimal".to_string())
-    install_environment("go-min".to_string())
 
+    if !args.search.is_empty() {
+        search(args.search)
+    }
+
+    if !args.beach_type.is_empty() {
+        setup_environment(args.beach_type)
+    }
+
+    if !args.install.is_empty() {
+        install_environment(args.install)
+    }
 }
