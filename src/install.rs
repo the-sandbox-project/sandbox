998dcce82bfc36cd3178f709a1288ce073740a52
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-26 21:45:46 +0100

    Feat(install.rs): New in_system Function

--- src/install.rs
+++ src/install.rs
@@ -1,12 +1,14 @@
-use std::fs;
+use std::process::exit;
+use std::path::Path;
 
 use crate::{get_path, id_is_valid, download::download_environment};
 
 pub async fn install_environment(id: String) {
     if id_is_valid(id.clone()) {
-        let environment_path = get_path(id.clone());
+        let environment_path = get_path(id.clone()).split("/").collect::<Vec<&str>>()[0].to_owned() + "/" + &id;
         if in_system(environment_path) {
-            println!("You already have {} installed on your system!", id)
+            println!("You already have {} installed on your system!", id);
+            exit(0)
         } else {
             download_environment(id).await.unwrap()
         }
@@ -18,10 +20,11 @@ pub async fn install_environment(id: String) {
 pub fn in_system(path: String) -> bool {
     let formatted_path = format!("/usr/share/sandbox/beaches/{}", path);
 
-    let installed = match fs::read(formatted_path) {
-        Ok(_) => true,
-        Err(_) => false
-    };
+    let path = Path::new(&formatted_path);
 
-    installed
+    if path.exists() && path.is_dir() {
+        true
+    } else {
+        false
+    }
 }
