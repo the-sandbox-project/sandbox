aa83fccd5ae61b16b1191ffa0a2ebb3ce127b4fe
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-27 16:46:18 +0100

    Refactor(search.rs): Support for Multiple Languages

--- src/search.rs
+++ src/search.rs
@@ -1,4 +1,4 @@
-use crate::{get_projects_list, install::in_system};
+use crate::{get_projects_list, install::in_system, get_templates_mapping};
 
 pub fn search(query: String) {
     println!("Environments That Match Query {}\n", query);
@@ -10,24 +10,24 @@ pub fn search(query: String) {
 
 pub fn search_environment(query: String) {
     let _sandbox_url = "https://raw.githubusercontent.com/the-sandbox-project/sandbox-templates/master/sandbox-templates.yml";
-    for (project_id, project) in get_projects_list() {
-        println!("project_id {:#?}", project_id);
-        let keywords = project["keywords"].as_str().unwrap_or("");
-        let keywords_list: Vec<&str> = keywords.split_whitespace().collect();
-        dbg!(&keywords_list);
+    for (_language, project) in get_templates_mapping() {
+        for (project_id, project_object) in project.as_mapping().unwrap() {
+            let keywords = project_object["keywords"].as_str().unwrap_or("");
+            let keywords_list: Vec<&str> = keywords.split_whitespace().collect();
 
-        for keyword in keywords_list {
-            if keyword == query {
-                let title = project["title"].as_str().unwrap_or("Title not Found");
-                let description = project["description"].as_str().unwrap_or("Description not Found");
-                let id = project_id.as_str().unwrap().to_string();
+            for keyword in keywords_list {
+                if keyword == query {
+                    let title = project_object["title"].as_str().unwrap_or("Title not Found");
+                    let description = project_object["description"].as_str().unwrap_or("Description not Found");
+                    let id = project_id.as_str().unwrap().to_string();
 
-                let installed = match in_system(id.clone()) {
-                    true => "✅",
-                    false => "❌"
-                };
+                    let installed = match in_system(id.clone()) {
+                        true => "✅",
+                        false => "❌"
+                    };
 
-                println!("     {} {} ({}) - {}", installed, title, id, description)
+                    println!("     {} {} ({}) - {}", installed, title, id, description)
+                }
             }
         }
     }
