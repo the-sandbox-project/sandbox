872c141b368609977ce2442d1eadfbdbd23f3664
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-27 11:16:45 +0100

    Feat(install): New in_system Function

--- src/install.rs
+++ src/install.rs
@@ -5,8 +5,7 @@ use crate::{get_path, id_is_valid, download::download_environment};
 
 pub async fn install_environment(id: String) {
     if id_is_valid(id.clone()) {
-        let environment_path = get_path(id.clone()).split("/").collect::<Vec<&str>>()[0].to_owned() + "/" + &id;
-        if in_system(environment_path) {
+        if in_system(id.clone()) {
             println!("You already have {} installed on your system!", id);
             exit(0)
         } else {
@@ -17,8 +16,9 @@ pub async fn install_environment(id: String) {
     }
 }
 
-pub fn in_system(path: String) -> bool {
-    let formatted_path = format!("/usr/share/sandbox/beaches/{}", path);
+pub fn in_system(id: String) -> bool {
+    let environment_path = get_path(id.clone()).split("/").collect::<Vec<&str>>()[0].to_owned() + "/" + &id;
+    let formatted_path = format!("/usr/share/sandbox/beaches/{}", environment_path);
 
     let path = Path::new(&formatted_path);
 
