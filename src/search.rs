7f35609154020268038f893bb489f7c6b9dbdc2e
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-27 11:16:29 +0100

    Refactor(search.rs): Use in_system Function Instead

--- src/search.rs
+++ src/search.rs
@@ -1,6 +1,4 @@
-use std::fs;
-
-use serde_yaml::Value;
+use crate::{get_projects_list, install::in_system};
 
 pub fn search(query: String) {
     println!("Environments That Match Query {}\n", query);
@@ -12,33 +10,24 @@ pub fn search(query: String) {
 
 pub fn search_environment(query: String) {
     let _sandbox_url = "https://raw.githubusercontent.com/the-sandbox-project/sandbox-templates/master/sandbox-templates.yml";
-    let sandbox_templates_path = "../sandbox-templates/sandbox-templates.yml";
+    for (project_id, project) in get_projects_list() {
+        println!("project_id {:#?}", project_id);
+        let keywords = project["keywords"].as_str().unwrap_or("");
+        let keywords_list: Vec<&str> = keywords.split_whitespace().collect();
+        dbg!(&keywords_list);
 
-    let file_contents = std::fs::read_to_string(sandbox_templates_path).unwrap();
-    let templates: Value = serde_yaml::from_str(&file_contents).unwrap();
+        for keyword in keywords_list {
+            if keyword == query {
+                let title = project["title"].as_str().unwrap_or("Title not Found");
+                let description = project["description"].as_str().unwrap_or("Description not Found");
+                let id = project_id.as_str().unwrap().to_string();
 
-    if let Some(languages) = templates["languages"].as_mapping() {
-        for (_language_name, projects) in languages {
-            if let Some(projects_list) = projects.as_mapping() {
-                for (project_id, project) in projects_list {
-                    let keywords = project["keywords"].as_str().unwrap_or("");
-                    let keywords_list: Vec<&str> = keywords.split_whitespace().collect();
+                let installed = match in_system(id.clone()) {
+                    true => "✅",
+                    false => "❌"
+                };
 
-                    for keyword in keywords_list {
-                        if keyword == query {
-                            let title = project["title"].as_str().unwrap_or("Title not Found");
-                            let description = project["description"].as_str().unwrap_or("Description not Found");
-                            let path = format!("/usr/share/sandbox/beaches/{}", project["path"].as_str().unwrap_or("Path not Found"));
-
-                            let installed = match fs::read(path) {
-                                Ok(_) => "✅",
-                                Err(_) => "❌"
-                            };
-
-                            println!("     {} {} ({}) - {}", installed, title, project_id.as_str().unwrap(), description)
-                        }
-                    }
-                }
+                println!("     {} {} ({}) - {}", installed, title, id, description)
             }
         }
     }
