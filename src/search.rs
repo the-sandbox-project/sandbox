3b7c219fcabd5573fbb19696a6790cdc8c4fbd08
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-29 22:50:33 +0100

    Feat(search.rs): Checks for No Keyword Matches

--- src/search.rs
+++ src/search.rs
@@ -1,17 +1,21 @@
 use crate::install::in_system;
 use crate::get_templates_mapping;
 
-pub fn search(query: String) {
-    println!("Environments That Match Query {}\n", query);
+pub async fn search(query: String) {
+    let matching_environments_exist = search_environment(query.clone()).await;
 
-    search_environment(query);
-
-    println!("\nInstall any of these with sandbox install <ENVIRONMENT>")
+    if matching_environments_exist {
+        println!("\nEnvironments That Match Query {}", query);
+        println!("Install any of these with sandbox install <ENVIRONMENT>")
+    } else {
+        println!("No Environments Found for Query: {}", query)
+    }
 }
 
-pub fn search_environment(query: String) {
-    let _sandbox_url = "https://raw.githubusercontent.com/the-sandbox-project/sandbox-templates/master/sandbox-templates.yml";
-    for (_language, project) in get_templates_mapping() {
+pub async fn search_environment(query: String) -> bool {
+    let mut matching_environments_exist = false;
+
+    for (_language, project) in get_templates_mapping().await.unwrap() {
         for (project_id, project_object) in project.as_mapping().unwrap() {
             let keywords = project_object["keywords"].as_str().unwrap_or("");
             let keywords_list: Vec<&str> = keywords.split_whitespace().collect();
@@ -22,15 +26,19 @@ pub fn search_environment(query: String) {
                     let description = project_object["description"].as_str().unwrap_or("Description not Found");
                     let id = project_id.as_str().unwrap().to_string();
 
-                    let installed = match in_system(id.clone()) {
+                    let installed = match in_system(id.clone()).await {
                         true => "✅",
                         false => "❌"
                     };
 
-                    println!("     {} {} ({}) - {}", installed, title, id, description)
+                    matching_environments_exist = true;
+
+                    println!("     {} {} ({}) - {}", installed, title, id, description);
+
                 }
             }
         }
     }
+    matching_environments_exist
 }
 
