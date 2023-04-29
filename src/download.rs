e2272eaf2f371bffda5223c0f85d665bb6f88c7b
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-29 22:49:11 +0100

    Refactor(download.rs): Changed tokio::fs => std::fs

--- src/download.rs
+++ src/download.rs
@@ -4,20 +4,20 @@ use std::io;
 use std::path::Path;
 use std::thread;
 use std::time::Duration;
+use std::fs;
 use std::{cmp::min, fmt::Write};
 
 
 use reqwest::Client;
 use flate2::read::GzDecoder;
 use tar::Archive;
-use tokio::fs;
 use indicatif::{ProgressBar, ProgressState, ProgressStyle};
 
 use crate::get_path;
 
 pub async fn download_environment(id: String) -> Result<(), Box<dyn Error>> {
     let base_path = "/usr/share/sandbox/beaches/";
-    let environment_path = get_path(id.clone());
+    let environment_path = get_path(id.clone()).await;
 
     let download_url = format!("https://github.com/the-sandbox-project/sandbox-templates/raw/master/{}", environment_path);
     let download_path = format!("{}{}", base_path, environment_path);
@@ -28,7 +28,7 @@ pub async fn download_environment(id: String) -> Result<(), Box<dyn Error>> {
 
     if response.status().is_success() {
         let language_path = Path::new(&download_path).parent().unwrap().to_str().unwrap();
-        fs::create_dir_all(language_path).await?;
+        fs::create_dir_all(language_path)?;
 
         let mut file = File::create(&download_path)?;
 
@@ -61,9 +61,9 @@ pub async fn download_environment(id: String) -> Result<(), Box<dyn Error>> {
         let mut archive = Archive::new(tar);
 
         archive.unpack(&unzip_path)?;
-        fs::remove_file(&download_path).await?;
+        fs::remove_file(&download_path)?;
 
-        println!("Installed {}! Run it with: sandbox --new {}", id, id)
+        println!("Installed {}! Test it out with: sandbox --new {}", id, id)
     }
     Ok(())
 }
