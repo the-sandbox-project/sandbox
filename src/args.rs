8b274309f585c39f897d6cf6a6cb561af42f803e
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-05-01 22:01:22 +0100

    Feat(args.rs): Added Uninstall Flag

--- src/args.rs
+++ src/args.rs
@@ -4,8 +4,12 @@ use clap::Parser;
 #[clap(author, version, about)]
 pub struct SandboxArgs {
     /// Create a New Environment
-    #[clap(short = 'n', long = "new", default_value = "")]
+    #[clap(short, long, default_value = "")]
     pub new: String,
+
+    /// Create a New Environment
+    #[clap(short = 'U', long, default_value = "")]
+    pub uninstall: String,
 
     /// Search for Environment
     #[clap(short = 'S', long, default_value = "")]
