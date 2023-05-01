a20fd6ea228a771fa0e4d12658cb07c9e3b69739
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-05-01 22:02:28 +0100

    Feat(install.rs): Install Function Naming

--- src/install.rs
+++ src/install.rs
@@ -1,31 +1,18 @@
-use std::path::Path;
-
 use colored::Colorize;
 
-use crate::{get_path, id_is_valid, download::download_environment};
+use crate::id_is_valid;
+use crate::download::install;
+use crate::in_system;
 
 pub async fn install_environment(id: String) {
     if id_is_valid(id.clone()).await {
         if in_system(id.clone()).await {
             println!("You already have {} installed on your system!", id.bright_green());
         } else {
-            download_environment(id.clone()).await.unwrap();
+            install(id.clone()).await.unwrap();
         }
     } else {
            println!("The environment ({}) does {} exist!", id.bright_green(), "not".red())
     }
 }
 
-pub async fn in_system(id: String) -> bool {
-    let path = get_path(id.clone()).await;
-    let environment_path = path.split("/").collect::<Vec<&str>>()[0].to_owned() + "/" + &id;
-    let formatted_path = format!("/usr/share/sandbox/beaches/{}", environment_path);
-
-    let path = Path::new(&formatted_path);
-
-    if path.exists() && path.is_dir() {
-        true
-    } else {
-        false
-    }
-}
